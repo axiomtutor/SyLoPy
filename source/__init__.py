2234c439a7895c43a0f6cc1391defb220f35f0ac
Adam Frank <axiomtutor@gmail.com>
2026-08-20 10:09:06 -0400

    activate ProofContext-backed validator

--- source/__init__.py
+++ source/__init__.py
@@ -5,3 +5,11 @@
 # separate from the parser implementation.
 from . import ProofParser as ProofParser
 from . import ProofParserPolicy as ProofParserPolicy
+
+# The proof validator is layered after ProofLogic has been loaded by
+# ProofParser.  This makes ProofContext the authoritative lexical environment
+# without introducing an import cycle into the kernel modules themselves.
+from .ContextProofValidator import ProofValidator as ContextProofValidator
+from . import ProofLogic as _ProofLogic
+
+_ProofLogic.ProofValidator = ContextProofValidator
